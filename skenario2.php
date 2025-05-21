5a9645025fa3ecfb1b57d6c907f52c6f9d5c6572
hizkia1437 <144123878+hizkia1437@users.noreply.github.com>
2025-05-21 14:31:51 +0700

    Update skenario2.php

--- skenario2.php
+++ skenario2.php
@@ -1,24 +1,4 @@
 <?php
-/*
-$nilai = 70;
-
-if($nilai <= 79 && $nilai >=70)
-{
-    echo "Grade: C = Calon bos engineer";
-}
-else if($nilai <= 100 && $nilai >=90){
-    echo "Grade: A = Anjay";
-}
-else if($nilai <= 89 && $nilai >=80){
-    echo "Grade: B = Baik";
-}
-else if ($nilai <=69 && $nilai > 0){
-    echo "Grade D = Dongo";
-}
-else {
-    echo "Nilai wajib diatas 0";
-}
-*/ 
 
 $jam = date("18");
 
